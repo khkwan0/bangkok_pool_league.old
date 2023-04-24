72d41797406ce1365011e91abf6da69b3095ec99
Ken <khkwan0@gmail.com>
2023-04-24 12:03:59 +0700

    remove some redux

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,11 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from './userSlice'
-import matchRefreshReducer from './matchRefreshSlice'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    matchRefresh: matchRefreshReducer,
   },
 })
 
